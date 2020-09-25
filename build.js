f7aa8c36fafe146375e999d6186a18fdf6a6a8f2
Lea Verou <lea@verou.me>
2020-09-25 19:44:03 +0300

    Generate nomodule version

---
+++ build.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+function readFile(file, enc = "utf8") {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, enc, (err,data) => {
+			if (err) {
+				reject(err);
+			}
+
+			resolve(data);
+		});
+	});
+}
+
+function writeFile(file, contents, enc) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(file, contents, enc, (err) => {
+			if (err) {
+				reject(err);
+			}
+
+			resolve();
+		});
+	});
+}
+
+(async () => {
+
+let data = await readFile("./parsel.js");
+let names = [];
+
+data = data.replace(/export function (\w+)/g, ($0, name) => {
+	names.push(name);
+	return "function " + name;
+});
+
+let contents = `(() => {
+${data}
+
+self.parsel = {${names.join(", ")}};
+})();`;
+
+writeFile("./parsel_nomodule.js", contents);
+
+})();
