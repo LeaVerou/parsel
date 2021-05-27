01b4891c9326855b5b4350c4de349be42dd932a4
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-05-27 18:09:20 +0530

    chore: add subfolder for cjs and umd

--- rollup.config.js
+++ rollup.config.js
@@ -4,33 +4,33 @@ export default {
   input: 'parsel.js',
   output: [
   {
-    file: 'dist/parsel.cmd.js',
+    file: 'dist/cjs/parsel.js',
     format: 'cjs'
   },
   {
-    file: 'dist/parsel.cmd.min.js',
+    file: 'dist/cjs/parsel.min.js',
     format: 'cjs',
     plugins: [ terser() ]
   },
   {
     name: 'parsel',
-    file: 'dist/parsel_nomodule.js',
+    file: 'dist/nomodule/parsel.js',
     format: 'iife'
   },
   {
     name: 'parsel',
-    file: 'dist/parsel_nomodule.min.js',
+    file: 'dist/nomodule/parsel.min.js',
     format: 'iife',
     plugins: [ terser() ]
   },
   {
     name: 'parsel',
-    file: 'dist/parsel.umd.js',
+    file: 'dist/umd/parsel.js',
     format: 'umd'
   },
   {
     name: 'parsel',
-    file: 'dist/parsel.umd.min.js',
+    file: 'dist/umd/parsel.min.js',
     format: 'umd',
     plugins: [ terser() ]
   },
