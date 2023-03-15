eaa9b52f23603f9b3a81d821cf0b14acd62c2761
Randolf <jrandolf@google.com>
2023-03-15 16:17:04 +0100

    Format rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,47 @@
-import { terser } from "rollup-plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
 	{
-		input: "parsel.ts",
+		input: 'parsel.ts',
 		output: [
 			{
-				file: "dist/cjs/parsel.js",
-				format: "cjs",
+				file: 'dist/cjs/parsel.js',
+				format: 'cjs',
 			},
 			{
-				name: "parsel",
-				file: "dist/nomodule/parsel.js",
-				format: "iife",
+				name: 'parsel',
+				file: 'dist/nomodule/parsel.js',
+				format: 'iife',
 			},
 			{
-				name: "parsel",
-				file: "dist/umd/parsel.js",
-				format: "umd",
+				name: 'parsel',
+				file: 'dist/umd/parsel.js',
+				format: 'umd',
 			},
 			{
-				file: "dist/parsel.js",
-				format: "es",
+				file: 'dist/parsel.js',
+				format: 'es',
 			},
 		].concat(
 			[
 				{
-					file: "dist/cjs/parsel.min.js",
-					format: "cjs",
+					file: 'dist/cjs/parsel.min.js',
+					format: 'cjs',
 				},
 				{
-					name: "parsel",
-					file: "dist/nomodule/parsel.min.js",
-					format: "iife",
+					name: 'parsel',
+					file: 'dist/nomodule/parsel.min.js',
+					format: 'iife',
 				},
 				{
-					name: "parsel",
-					file: "dist/umd/parsel.min.js",
-					format: "umd",
+					name: 'parsel',
+					file: 'dist/umd/parsel.min.js',
+					format: 'umd',
 				},
 				{
-					file: "dist/parsel.min.js",
-					format: "es",
+					file: 'dist/parsel.min.js',
+					format: 'es',
 				},
 			].map((output) => Object.assign(output, { plugins: [terser()] }))
 		),
